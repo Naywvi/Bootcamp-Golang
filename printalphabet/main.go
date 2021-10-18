cc33b2447f0416dd88bdcea07e35315796bcb05d
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-18 08:53:09 +0200

    fix

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -6,5 +6,5 @@ func main() {
 	for compteur := 97; compteur < 123; compteur++ { //Boucle appelé compteur allant de 97 à &é"
 		z01.PrintRune(rune(compteur)) //Impréssion de la boucle en ASCII
 	}
-	z01.PrintRune(rune('\n'))
+	z01.PrintRune('\n')
 }
