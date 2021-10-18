c7b68cffab53ae0286533e05a6930aeaf17e09fd
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-18 19:20:49 +0200

    for range string>compteur

--- strlen.go
+++ strlen.go
@@ -1,18 +1,9 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-func StrLen(s string) {
-	var w int = 0
-	for _, s := range s {
-		w++
-		if s == 1 {
-			w++
-			s = 12
-		}
-		if w == 12 {
-			z01.PrintRune(49 + 50)
-		}
-
+func StrLen(s string) int {
+	l := 0
+	for range s {
+		l++
 	}
+	return l
 }
