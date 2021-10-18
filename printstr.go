1e825ca98d7365cbc27fae0a9a518b7c64d0012e
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-18 17:39:11 +0200

    convert string>int/runeASCII

--- printstr.go
+++ printstr.go
@@ -1,6 +1,11 @@
 package piscine
 
+import (
+	"github.com/01-edu/z01"
+)
+
 func PrintStr(s string) {
-	s = "Hello world !"
-	println(s)
+	for _, convert := range s {
+		z01.PrintRune(convert)
+	}
 }
