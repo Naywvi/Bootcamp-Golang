80e37fbcb3ece509dc1537fbdc58a5c8ea508e2b
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-18 22:09:40 +0200

    a revoir l'algo

--- strrev.go
+++ strrev.go
@@ -1,6 +1,9 @@
 package piscine
 
 func StrRev(s string) string {
-	s = "!dlroW olleH"
-	return s
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
 }
