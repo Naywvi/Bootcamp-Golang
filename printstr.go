34ff6162553c1c4308dccfdbbfed28b03c497ceb
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-18 11:52:45 +0200

    test

---
+++ printstr.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+
+	z01.PrintRune(rune(72))
+	z01.PrintRune(rune(101))
+	z01.PrintRune(rune(108))
+	z01.PrintRune(rune(108))
+	z01.PrintRune(rune(111))
+	z01.PrintRune(rune(32))
+	z01.PrintRune(rune(119))
+	z01.PrintRune(rune(111))
+	z01.PrintRune(rune(114))
+	z01.PrintRune(rune(100))
+	z01.PrintRune(rune(33))
+	z01.PrintRune(rune('\n'))
+}
