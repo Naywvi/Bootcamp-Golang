e52d4bf7c66927ab66588689202d2ca96ccf1fef
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-18 12:11:08 +0200

    Test

--- printstr.go
+++ printstr.go
@@ -1,18 +1,25 @@
-package piscine
+package main
 
-import "github.com/01-edu/z01"
+import (
+	"fmt"
+)
 
 func PrintStr(s string) {
-	z01.PrintRune(rune(72))
-	z01.PrintRune(rune(101))
-	z01.PrintRune(rune(108))
-	z01.PrintRune(rune(108))
-	z01.PrintRune(rune(111))
-	z01.PrintRune(rune(32))
-	z01.PrintRune(rune(119))
-	z01.PrintRune(rune(111))
-	z01.PrintRune(rune(114))
-	z01.PrintRune(rune(100))
-	z01.PrintRune(rune(33))
-	z01.PrintRune(rune('\n'))
+	s = "Hello world !"
+	fmt.Println(s)
+	// z01.PrintRune(rune(72))
+	// z01.PrintRune(rune(101))
+	// z01.PrintRune(rune(108))
+	// z01.PrintRune(rune(108))
+	// z01.PrintRune(rune(111))
+	// z01.PrintRune(rune(32))
+	// z01.PrintRune(rune(119))
+	// z01.PrintRune(rune(111))
+	// z01.PrintRune(rune(114))
+	// z01.PrintRune(rune(100))
+	// z01.PrintRune(rune(33))
+	// z01.PrintRune(rune('\n'))
+}
+func main() {
+	PrintStr("Hello world!")
 }
