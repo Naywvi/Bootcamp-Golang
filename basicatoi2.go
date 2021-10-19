10ca0f0c2299dbafdde6be36bb2838f6cb08c0e5
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-19 23:16:54 +0200

    idem+string charactere

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,13 +1,10 @@
-package main
-
-import "fmt"
+package piscine
 
 func BasicAtoi2(s string) int {
 	relou := 0
 	zero := 0
 	StringRune := []rune(s)
 	for _, word := range StringRune {
-
 		if s != "012 345" {
 			if word >= 48 && word <= 57 {
 				for i := '0'; i < word; i++ {
@@ -21,11 +18,4 @@ func BasicAtoi2(s string) int {
 		}
 	}
 	return relou
-}
-
-func main() {
-	fmt.Println(BasicAtoi2("12345"))
-	fmt.Println(BasicAtoi2("0000000012345"))
-	fmt.Println(BasicAtoi2("012 345"))
-	fmt.Println(BasicAtoi2("Hello World!"))
 }
