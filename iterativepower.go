24987ff71f1a21c6fd82e31cbe74160e67cc9cf7
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-22 00:37:40 +0200

    puissances

--- iterativepower.go
+++ iterativepower.go
@@ -3,11 +3,9 @@ package piscine
 func IterativePower(nb int, power int) int {
 	x := *&nb
 	if nb == 0 || power == 0 {
-		println("NUL DONC ZERO", nb, power)
 		return 0
 	}
 	if nb < 0 || power < 0 {
-		println("ZERO", nb, power)
 		return 0
 	}
 	for boucle := 0; boucle < power-1; boucle++ {
