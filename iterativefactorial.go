7ce2f2925417bb69ef2db96874f218b426f03b57
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-21 14:09:46 +0200

    fix factorial error 400

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,7 +3,7 @@ package piscine
 func IterativeFactorial(nb int) int {
 	c := nb
 	for boucle := 1; boucle < nb; boucle++ {
-		if boucle > 24 || boucle == 0 {
+		if nb > 24 || nb <= 0 {
 			return 0
 		} else if boucle > 0 || boucle > 24 {
 			c = c * boucle
