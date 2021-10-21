d8f508159b082128917e23033ae1f8e6239059e9
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-21 12:22:46 +0200

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,10 +2,9 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	c := nb
-	for boucle := 0; boucle < nb; boucle++ {
-		if boucle == 0 {
-			boucle++
-		} else {
+
+	for boucle := 1; boucle < nb; boucle++ {
+		if nb < 0 || nb > 99 {
 			c = c * boucle
 		}
 	}
