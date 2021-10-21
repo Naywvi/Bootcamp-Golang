9870de27886482ecdae352d8a5828fe78c8355c7
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-21 12:05:33 +0200

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,8 +5,10 @@ func IterativeFactorial(nb int) int {
 	for boucle := 0; boucle < nb; boucle++ {
 		if boucle == 0 {
 			boucle++
+		} else {
+			c = c * boucle
 		}
-		c = c * boucle
+
 	}
 	return c
 }
