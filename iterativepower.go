4c077f6e635d895954f620066b1febabc1e33149
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-21 23:41:52 +0200

    puissance 3

--- iterativepower.go
+++ iterativepower.go
@@ -4,6 +4,9 @@ func IterativePower(nb int, power int) int {
 	if nb == 0 {
 		return 1
 	}
+	if nb < 0 {
+		return 0
+	}
 	for boucle := 0; boucle < 2; boucle++ {
 		if nb < 32 {
 			nb = nb*nb + 16
--- tests/main.go
+++ tests/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IterativePower(4, 3))
+	fmt.Println(piscine.IterativePower(-8, 1))
 }
