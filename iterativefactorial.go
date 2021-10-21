e5f14d74ac374690e216cb8e4d7b4cbe0f148df7
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-21 14:06:03 +0200

    fix factorial error 400

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,7 +5,7 @@ func IterativeFactorial(nb int) int {
 	for boucle := 1; boucle < nb; boucle++ {
 		if boucle > 24 || boucle == 0 {
 			return 0
-		} else if boucle > 0 || boucle > 24 {
+		} else if boucle < 0 || boucle > 24 {
 			c = c * boucle
 		}
 	}
