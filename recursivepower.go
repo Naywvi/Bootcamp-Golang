96c6ef810bab4a1d508e42b629ee2b3ef2bbbacd
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-22 09:10:24 +0200

    Recursive(return func)

--- recursivepower.go
+++ recursivepower.go
@@ -1,12 +1,12 @@
 package piscine
 
-func ReccursivePower(nb int, power int) int {
+func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	}
 	if power < 0 {
 		return 0
 	} else {
-		return nb * ReccursivePower(nb, power-1)
+		return nb * RecursivePower(nb, power-1)
 	}
 }
