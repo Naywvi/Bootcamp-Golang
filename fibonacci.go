e8e06fec6863dffc1821f19739f1d94abeb477a3
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-23 17:05:27 +0200

    La suite fibonnaci recursive

---
+++ fibonacci.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		print("\nJe passe ici\n", index)
+		return -1
+	}
+	if index == 0 {
+		print("\nJe passe ici\n", index)
+		return 0
+	}
+	if index == 1 {
+		print("\nJe passe ici\n", index)
+		return 1
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2)
+
+}
