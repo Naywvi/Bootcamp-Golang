9d589abe97da00487a53174c4ff9d0b788aab538
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-24 17:01:20 +0200

    Nombre premier trier a partir de nombre composées

--- isprime.go
+++ isprime.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrime(nb int) bool {
 	onsenfou := true
-	for boucle := 9; boucle > 1; boucle-- {
+	for boucle := 90; boucle > 1; boucle-- {
 		y := boucle * boucle
 		if y == nb {
 			return false
