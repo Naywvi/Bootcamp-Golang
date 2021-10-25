adecdda0cd415a543526618c30558799068570f6
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-25 12:12:03 +0200

    next prime | Composée +1

--- findnextprime.go
+++ findnextprime.go
@@ -1,30 +1,29 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb > 0 {
-		if nb == 2 || nb == 3 {
-			return nb
+	if nb < 0 {
+		return 1
+	}
+	if nb == 2 || nb == 3 {
+		return nb
+	}
+	for a := 2; a < nb; a++ {
+		if nb%a == 0 {
+			return 0
 		}
-		for a := 2; a < nb; a++ {
-			if nb%a == 0 {
-				return 0
-			}
-			if nb%3 == 0 || nb%2 == 0 || nb%5 == 0 {
-				for i := 0; i < nb; i++ {
+		if nb%3 == 0 || nb%2 == 0 || nb%5 == 0 {
+			for i := 0; i < nb; i++ {
 
-					x := 9
-					x = nb + 1
+				x := 9
+				x = nb + 1
 
-					if nb%3 == 0 || nb%2 == 0 || nb%5 == 0 {
-						return FindNextPrime(x)
-					}
-					return x
+				if nb%3 == 0 || nb%2 == 0 || nb%5 == 0 {
+					return FindNextPrime(x)
 				}
+				return x
 			}
-			return nb
 		}
-	} else {
-		return 0
+		return nb
 	}
 	return nb
 }
