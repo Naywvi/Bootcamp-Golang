a7075593a125931df2826c068f16810e8113bd0f
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-25 15:21:48 +0200

    n vers str(rune)

---
+++ nrune.go
@@ -0,0 +1,17 @@
+package piscine
+
+func NRune(s string, n int) rune {
+
+	x := []rune(s)
+	y := 0
+	if n < 0 {
+		return 0
+	}
+	for i := range x {
+		y = i + 1
+		if y == n {
+			return x[n-1]
+		}
+	}
+	return 0
+}
