4864933c773bc063e6964722ea259e4d3574828f
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-25 12:08:59 +0200

    next prime | Composée +1

--- findnextprime.go
+++ findnextprime.go
@@ -5,19 +5,24 @@ func FindNextPrime(nb int) int {
 		if nb == 2 || nb == 3 {
 			return nb
 		}
-		if nb%3 == 0 || nb%2 == 0 {
-			for i := 0; i < nb; i++ {
-
-				x := 9
-				x = nb + 1
-
-				if nb%3 == 0 || nb%2 == 0 {
-					return FindNextPrime(x)
-				}
-				return x
+		for a := 2; a < nb; a++ {
+			if nb%a == 0 {
+				return 0
 			}
+			if nb%3 == 0 || nb%2 == 0 {
+				for i := 0; i < nb; i++ {
+
+					x := 9
+					x = nb + 1
+
+					if nb%3 == 0 || nb%2 == 0 {
+						return FindNextPrime(x)
+					}
+					return x
+				}
+			}
+			return nb
 		}
-		return nb
 	} else {
 		return 0
 	}
