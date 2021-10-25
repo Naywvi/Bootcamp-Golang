3bb7b38faec129c21ddd2cc4a2e2c0999c1682f4
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-25 09:57:34 +0200

    nombres premiers et divisions

--- isprime.go
+++ isprime.go
@@ -1,18 +1,23 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	onsenfou := true
-	for boucle := 90; boucle > 1; boucle-- {
-		y := boucle * boucle
-		if y == nb {
+	if nb > 0 {
+		if nb == 1 {
 			return false
 		}
-		if y < nb {
+		if nb == 2 || nb == 3 {
 			return true
 		}
-		if nb < 0 {
+		if nb%3 == 0 || nb%2 == 0 {
 			return false
 		}
+		for a := 2; a < nb; a++ {
+			if nb%a == 0 {
+				return false
+			}
+		}
+		return true
+	} else {
+		return false
 	}
-	return onsenfou
 }
