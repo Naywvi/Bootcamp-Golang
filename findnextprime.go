329f89dd0410d8f0f51bcf4b314f5a6b60af2855
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-25 12:07:30 +0200

    next prime | Composée +1

---
+++ findnextprime.go
@@ -0,0 +1,24 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb > 0 {
+		if nb == 2 || nb == 3 {
+			return nb
+		}
+		if nb%3 == 0 || nb%2 == 0 {
+			for i := 0; i < nb; i++ {
+
+				x := 9
+				x = nb + 1
+
+				if nb%3 == 0 || nb%2 == 0 {
+					return FindNextPrime(x)
+				}
+				return x
+			}
+		}
+		return nb
+	} else {
+		return 0
+	}
+}
