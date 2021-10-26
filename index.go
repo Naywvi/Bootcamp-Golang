9faab1267449a0b389b92deee8363c9f5cc1bc36
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-26 16:06:41 +0200

    Comparaison entre deux string et print le [?]

--- index.go
+++ index.go
@@ -1,16 +1,9 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	r := []rune(s)
-	r2 := []rune(toFind)
 	for i := range s {
-		for j := range toFind {
-			if r[i] == r2[j] {
-				if r[0] == r2[j] {
-					return -1
-				}
-				return i
-			}
+		if i+len(toFind) <= len(s) && string(s[i:i+len(toFind)]) == toFind {
+			return i
 		}
 	}
 	return -1
