c0687db57ffad6f609097f8d62f63786595c2a1b
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-27 13:59:30 +0200

    réordonner int en ordre croissant

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -7,12 +7,19 @@ func PrintNbrInOrder(n int) {
 		z01.PrintRune(rune(48))
 	}
 	if n > 0 {
-		for boucle := 0; boucle < 3; boucle++ {
-			r := n / 10
-			x := n % 10
-			n = r
-			z01.PrintRune(rune(48 + x))
+		if n > 0 {
+			for boucle2 := n; boucle2 > 0; boucle2-- {
+				for boucle := 0; boucle < 3; boucle++ {
+					r := n / 10
+					x := n % 10
+					n = r
+					z01.PrintRune(rune(48 + x))
+				}
+				if n == 0 {
+					z01.PrintRune('\n')
+					return
+				}
+			}
 		}
-		z01.PrintRune('\n')
 	}
 }
