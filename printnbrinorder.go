d4a360714c62cbf07943639b6b049db446b20043
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-10-27 20:01:46 +0200

    A REVOIR, changement de place [?]>[?] HORRIBLE

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -7,19 +7,31 @@ func PrintNbrInOrder(n int) {
 		z01.PrintRune(rune(48))
 	}
 	if n > 0 {
-		if n > 0 {
-			for boucle2 := n; boucle2 > 0; boucle2-- {
-				for boucle := 0; boucle < 3; boucle++ {
-					r := n / 10
-					x := n % 10
-					n = r
-					z01.PrintRune(rune(48 + x))
-				}
-				if n == 0 {
-					z01.PrintRune('\n')
-					return
+		var SLICE []int
+		PLACER := 0
+		ONLEBOUGE := 0
+		Change := 0
+		for n != 0 {
+			PLACER = n % 10
+			n /= 10
+			SLICE = append(SLICE, PLACER)
+		}
+		for count := range SLICE {
+			ONLEBOUGE = count + 1
+		}
+		for i := 0; i < ONLEBOUGE-1; i++ {
+
+			for j := 0; j < ONLEBOUGE-i-1; j++ {
+				if SLICE[j] > SLICE[j+1] {
+					print("je passe ici")
+					Change = SLICE[j]
+					SLICE[j] = SLICE[j+1]
+					SLICE[j+1] = Change
 				}
 			}
+		}
+		for i := 0; i < ONLEBOUGE; i++ {
+			z01.PrintRune(rune(SLICE[i] + 48))
 		}
 	}
 }
