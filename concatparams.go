2505afc11169686b0942e88ed638bff959734817
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-02 10:40:52 +0100

    Concat slice == stack string

--- concatparams.go
+++ concatparams.go
@@ -3,8 +3,12 @@ package piscine
 func ConcatParams(args []string) string {
 	line := "\n"
 	str := ""
-	for Boucle := 0; Boucle < len(args); Boucle++ {
-		str += (args[Boucle] + line)
+	for i := 0; i < len(args); i++ {
+		if i != len(args)-1 {
+			str += (args[i] + line)
+		} else {
+			str += (args[i])
+		}
 	}
 	return str
 }
