ced684eb57a3fffd7b24c7e14dab7ce77a3dc840
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-03 13:02:53 +0100

    Print nbr/nbr neg +lim neg via RUNE MOD SLICE

--- printnbr.go
+++ printnbr.go
@@ -6,7 +6,7 @@ func PrintNbr(n int) {
 	if n == 0 {
 		z01.PrintRune(rune(48))
 	}
-	if n < 0 {
+	if n < 0 && n != -9223372036854775808 {
 		if n < 0 {
 			n *= -1
 		}
@@ -40,6 +40,25 @@ func PrintNbr(n int) {
 				return
 			}
 		}
+		nr := len(SLICE)
+		for i := nr - 1; i > 0; i-- {
+			z01.PrintRune(rune(SLICE[i] + 48))
+		}
+		z01.PrintRune(rune(SLICE[0] + 48))
+	} else if n == -9223372036854775808 {
+		SLICE := []int{}
+		ns := 0
+
+		for n != 0 {
+			if n != 0 {
+				ns = n % 10 * (-1)
+				n /= 10
+				SLICE = append(SLICE, ns)
+			} else {
+				return
+			}
+		}
+		z01.PrintRune(rune(45))
 		nr := len(SLICE)
 		for i := nr - 1; i > 0; i-- {
 			z01.PrintRune(rune(SLICE[i] + 48))
