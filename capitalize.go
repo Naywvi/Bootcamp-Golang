cc2b66b5c8d1e9005b42015e199249a374f194a4
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-04 18:31:55 +0100

    string > []rune(s) | check s[i] si autre caract de range (a-z et A-Z et 0-9)alors s[i]-32 | si s[i-1] = range alors s[i] min

--- capitalize.go
+++ capitalize.go
@@ -5,15 +5,22 @@ func Capitalize(s string) string {
 	count := 0
 	for i := 0; i < len(s); i++ {
 		count++
-		if srune[0] >= 'a' && srune[0] <= 'z' || srune[0] >= '0' && srune[0] <= '9' {
+		if srune[0] >= 'a' && srune[0] <= 'z' {
 			srune[i] = srune[i] - 32
 		}
 		if srune[i] >= 'a' && srune[i] <= 'z' {
-			if srune[i-1] >= 'a' && srune[i-1] <= 'z' || srune[i-1] >= 'A' && srune[i-1] <= 'Z' || srune[i-1] >= '0' && srune[i-1] <= '9' {
-			} else {
+			if false == (srune[i-1] >= 'a' && srune[i-1] <= 'z' || srune[i-1] >= 'A' && srune[i-1] <= 'Z' || srune[i-1] >= '0' && srune[i-1] <= '9') {
 				srune[i] = srune[i] - 32
 			}
 		}
+		if srune[i] != srune[0] {
+			if srune[i] >= 'A' && srune[i] <= 'Z' {
+				if srune[i-1] >= 'a' && srune[i-1] <= 'z' || srune[i-1] >= 'A' && srune[i-1] <= 'Z' {
+					srune[i] = srune[i] + 32
+				}
+			}
+		}
+
 	}
 	test := string(srune)
 	return test
