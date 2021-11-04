ead26eb35d0cda803a49b33b9586202c20db4686
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-04 19:45:48 +0100

    idem ISPRIME just add return func(nb+1)

--- findnextprime.go
+++ findnextprime.go
@@ -1,27 +1,17 @@
 package piscine
 
 func FindNextPrime(nb int) int {
+	if nb < 0 || nb == 1 || nb == 3 {
+		return 2
+	} else if nb == 3 {
+		return 3
+	}
 	if nb%3 == 0 || nb%2 == 0 {
-		if nb < 0 {
-			return 2
-		}
-		if nb == 1 {
-			return 2
-		}
-		if nb == 2 || nb == 3 {
-			return 2
-		}
-		if nb == 3 {
-			return 3
-		}
-		if nb%3 == 0 || nb%2 == 0 {
-			for a := 2; a < nb; a++ {
-				if nb%a == 0 {
-					return FindNextPrime(nb + 1)
-				}
+		for a := 2; a < nb; a++ {
+			if nb%a == 0 {
+				return FindNextPrime(nb + 1)
 			}
 		}
-		return nb
 	}
 	return nb
 }
