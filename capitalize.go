96b26dd032c76877103f6f4709b1af5459acb2eb
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-04 16:52:29 +0100

    string > []rune(s) | check s[i] si autre caract de range (a-z et A-Z)alors s[i]-32

--- capitalize.go
+++ capitalize.go
@@ -10,7 +10,6 @@ func Capitalize(s string) string {
 		}
 		if srune[i] >= 'a' && srune[i] <= 'z' {
 			if srune[i-1] >= 'a' && srune[i-1] <= 'z' || srune[i-1] >= 'A' && srune[i-1] <= 'Z' {
-
 			} else {
 				srune[i] = srune[i] - 32
 			}
