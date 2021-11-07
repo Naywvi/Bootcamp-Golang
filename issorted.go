a2b8841b321bcc4d8c1ee4ebe3499cfc02a0af1a
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-07 23:18:02 +0100

    x

--- issorted.go
+++ issorted.go
@@ -4,6 +4,8 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a); i++ {
 		if i+1 < len(a) && f(a[i], a[i+1]) == -1 {
 			return false
+		} else if i+1 < len(a) && f(a[i], a[i+1]) == 0 {
+			return false
 		}
 	}
 	return true
