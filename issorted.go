95df0d852876271be78949ba7374213921c454cf
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-07 23:22:51 +0100

    x

--- issorted.go
+++ issorted.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := len(a) - 1; i > 0; i-- {
-		if f(a[i], a[i-1]) == -1 {
+		if f(a[i], a[i-1]) == -1 || f(a[i-1], a[i]) == -1 {
 			return false
 		} else if f(a[i], a[i-1]) == 0 {
 			return false
