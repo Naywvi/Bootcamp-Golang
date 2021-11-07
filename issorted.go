8ee7a5d84a01a54fcde2d170bbb15eb90440da0b
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-07 23:15:57 +0100

    x

--- issorted.go
+++ issorted.go
@@ -4,11 +4,21 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	SLICEINT := 0
 	SLICEINT2 := 0
 	count1 := 0
+	finish := "1"
 	for i := len(a) - 2 + 1; i > 0; i-- {
 		SLICEINT = a[i]
 		SLICEINT2 = a[i-1]
 		if f(SLICEINT, SLICEINT2) == 1 {
 			count1++
+		}
+		if i == 1 {
+			for I := range finish {
+				SLICEINT = a[I]
+				SLICEINT2 = a[I]
+				if f(SLICEINT, SLICEINT2) == 1 {
+					count1++
+				}
+			}
 		}
 	}
 	if count1 == len(a)-1 {
