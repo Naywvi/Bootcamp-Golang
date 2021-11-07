28996744ac09ce0bcba8935dcbf23fe4c4484607
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-07 23:17:04 +0100

    x

--- issorted.go
+++ issorted.go
@@ -1,28 +1,10 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	SLICEINT := 0
-	SLICEINT2 := 0
-	count1 := 0
-	finish := "1"
-	for i := len(a) - 2 + 1; i > 0; i-- {
-		SLICEINT = a[i]
-		SLICEINT2 = a[i-1]
-		if f(SLICEINT, SLICEINT2) == 1 {
-			count1++
-		}
-		if i == 1 {
-			for I := range finish {
-				SLICEINT = a[I]
-				SLICEINT2 = a[I]
-				if f(SLICEINT, SLICEINT2) == 1 {
-					count1++
-				}
-			}
+	for i := 0; i < len(a); i++ {
+		if i+1 < len(a) && f(a[i], a[i+1]) == -1 {
+			return false
 		}
 	}
-	if count1 == len(a)-1 {
-		return true
-	}
-	return false
+	return true
 }
