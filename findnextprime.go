ac549b3823f6c6df978535b134b375f7890ddbbf
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-08 11:02:01 +0100

    find next with loop and lim

--- findnextprime.go
+++ findnextprime.go
@@ -1,19 +1,19 @@
 package piscine
 
 func FindNextPrime(nb int) int {
+	lim := 999999194
 	if nb > 0 {
-		if nb == 1 {
-			return 2
-		}
-		if nb == 2 {
+		if nb == (1 | 2) {
 			return 2
 		}
 		if nb == 3 {
 			return 3
 		}
-		for a := 2; a < nb; a++ {
-			if nb%a == 0 {
-				return FindNextPrime(nb + 1)
+		if nb < lim {
+			for a := 2; a < nb; a++ {
+				if nb%a == 0 {
+					return FindNextPrime(nb + 1)
+				}
 			}
 		}
 		return nb
