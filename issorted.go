e20194b4d2ad5f086161c16f6dcfb8a514353faa
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-08 09:01:01 +0100

    AREVOIR

--- issorted.go
+++ issorted.go
@@ -1,14 +1,21 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	count := 0
-	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) == 1 {
-			count++
+	if len(a) > 1 {
+		if f(a[0], a[1]) <= 0 {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) > 0 {
+					return false
+				}
+			}
+		}
+		if f(a[0], a[1]) > 0 {
+			for i := 0; i < len(a)-1; i++ {
+				if f(a[i], a[i+1]) < 0 {
+					return false
+				}
+			}
 		}
 	}
-	if count == len(a)-2 {
-		return true
-	}
-	return false
+	return true
 }
