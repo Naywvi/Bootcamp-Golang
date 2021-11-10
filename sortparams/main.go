744d496d6c92ddfc0bddd5747662dbbfd681fed5
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-10 20:57:44 +0100

    EZ

---
+++ sortparams/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	SLICE := []rune{}
+	arg := os.Args[1:]
+	str := ""
+	Change := '0'
+	for _, x := range arg {
+		str = str + x //vers string
+	}
+	for _, x := range str {
+		SLICE = append(SLICE, x)
+	}
+	for i := 0; i < len(arg); i++ {
+		for j := 0; j < len(arg)-i-1; j++ {
+			if SLICE[j] > SLICE[j+1] {
+				Change = SLICE[j]
+				SLICE[j] = SLICE[j+1]
+				SLICE[j+1] = Change
+			}
+		}
+	}
+	for i := 0; i < len(SLICE); i++ {
+		z01.PrintRune(SLICE[i])
+		z01.PrintRune('\n')
+	}
+}
