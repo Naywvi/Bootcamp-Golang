d4c4a50beae72fbc694bb5e950e48f8ef634e5e4
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-11 02:53:44 +0100

    ez

---
+++ printwordstables.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintWordsTables(a []string) {
+	RSLICE := []rune{}
+	str := ""
+	for i := range a {
+		str = a[i] + " "
+		for _, x := range str {
+			RSLICE = append(RSLICE, x)
+		}
+		str = ""
+	}
+	for i := 0; i < len(RSLICE)-1; i++ {
+		if RSLICE[i] == ' ' {
+			z01.PrintRune('\n')
+		} else {
+			z01.PrintRune(RSLICE[i])
+		}
+	}
+}
