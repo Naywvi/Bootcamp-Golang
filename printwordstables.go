9ce5ee419e6e9a770de0cb41b2b90f3b7f08f90b
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-11 15:03:41 +0100

    ez

--- printwordstables.go
+++ printwordstables.go
@@ -3,20 +3,18 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
-	RSLICE := []rune{}
-	str := ""
+	count := 0
 	for i := range a {
-		str = a[i] + " "
-		for _, x := range str {
-			RSLICE = append(RSLICE, x)
-		}
-		str = ""
-	}
-	for i := range RSLICE {
-		if RSLICE[i] == ' ' {
-			z01.PrintRune('\n')
-		} else {
-			z01.PrintRune(RSLICE[i])
+		count++
+		r := string(a[i])
+		for o, x := range r {
+			if count == 1 && o < len(r)-1 {
+				z01.PrintRune(x)
+			} else if o == len(r)-1 {
+				z01.PrintRune(x)
+				z01.PrintRune('\n')
+				count = 0
+			}
 		}
 	}
 }
