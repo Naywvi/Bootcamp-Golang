fe1dfef37d5b2c8f96bd74d622a94b1e8705223a
NLKAHDARI <nagib.lakhdari@ynov.com>
2021-11-15 09:30:07 +0100

    x

--- displayfile/main.go
+++ displayfile/main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 2 {
-		file, _ := os.Open(os.Args[1])
-		ioutil.ReadAll(file)
-	} else if len(os.Args) < 2 {
+	args := os.Args[1]
+
+	if len(os.Args) < 2 {
 		fmt.Println("File name missing")
 	} else if len(os.Args) > 2 {
 		fmt.Println("Too many arguments")
 	}
+	file, _ := ioutil.ReadFile(args)
+	fmt.Print(string(file[2:]))
 }
